8412504191844402b3888e25daecf010ba843eaa
VAUBAN_B2\Administrateur <qb.briere@gmail.com>
2023-06-14 09:48:09 +0200

    Commit 1

---
+++ token/src/main/java/token/Main/Main.java
@@ -0,0 +1,41 @@
+package token.Main;
+/**
+ * 
+ * @author Administrateur
+ *
+ */
+public class Main {
+/**
+ * 
+ * @param args	OK
+ */
+	
+	public static void main(String[] args) {
+		/**
+		 * OK
+		 */
+		Transform georgeTransformer = new Transform();
+		Transform erikaMault = new Transform();
+		
+		georgeTransformer.addToken("abbr", "M");
+		georgeTransformer.addToken("name", "Transformateur");
+		georgeTransformer.addToken("firstname", "Georges");
+		georgeTransformer.addToken("ville", "Lille");
+		georgeTransformer.addToken("address", "18, rue des Lilas");
+		georgeTransformer.addToken("postalCode", "59000");
+		
+		erikaMault.addToken("abbr", "Mme");
+		erikaMault.addToken("name", "Mault");
+		erikaMault.addToken("firstname", "Erika");
+		erikaMault.addToken("ville", "Poitiers");
+		erikaMault.addToken("address", "45, avenue de la Liberation");
+		//erikaMault.addToken("postalCode", "86000");
+		
+		String adMail = "Bonjour {abbr}. {name} {firstname}, nous savons que vous habitez le {address}, dans la ville de {ville}, {postalCode} !\nNous vous trouverons !";
+		
+		georgeTransformer.transformer(adMail);
+		erikaMault.transformer(adMail);
+	}
+
+}
+
---
+++ token/src/main/java/token/Main/Transform.java
@@ -0,0 +1,68 @@
+package token.Main;
+
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Transforms a text by replacing a set of tokens by
+ * pre-defined values
+ * 
+ * Can add or remove some value, Key and value, for the user with addToken
+ * 
+ * @author Administrateur
+ *
+ */
+
+public class Transform {
+	
+	private HashMap<String, String> person = new HashMap<String, String>();
+	
+	/**
+	 * 
+	 * @param theme	Name of the key, identifier for the token replacement
+	 * @param value	The value for the replacement
+	 */
+	public void addToken(String theme, String value){
+		person.put(theme, value);
+	}
+	
+	/**
+	 * 
+	 * @param theme	Name of the key, you want to remove from the Hashmap
+	 */
+	void removeToken(String theme) {
+		person.remove(theme);
+	}
+	
+	/**
+	 * 
+	 * @param input	The input text for the replacement, which include the tokens.
+	 * 				Take the token in between brackets {} and replace it with the value, 
+	 * 				which corresponds to key of our User Hashmap
+	 * 
+	 * 				ex: Hello {firstname} ! => Hello person.get("firstname") !
+	 */
+	void transformer(String input) {
+		
+		Pattern p = Pattern.compile("\\{(.*?)\\}");
+		Matcher m = p.matcher(input);
+		
+		StringBuffer sBuffer = new StringBuffer();
+		
+		while(m.find()) {
+			String keyString = (String) (m.group().subSequence(1,( m.group().length()-1)));
+			String value = person.get(keyString);
+			
+			if(value != null) {
+				m.appendReplacement(sBuffer, value);
+			}
+		}
+		
+		m.appendTail(sBuffer);
+		
+		
+		System.out.println(sBuffer);
+	}
+	
+}
